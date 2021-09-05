a60d152a5c643e60adcae977aeecf1ea03a3a8bd
Varshita Patakottu <varshita13@gmail.com>
2021-09-05 18:48:15 -0400

    added mobile version of nature card display

--- src/Components/NatureCardDisplay.jsx
+++ src/Components/NatureCardDisplay.jsx
@@ -25,7 +25,11 @@ const NatureCardDisplay = () => {
   ];
   return (
     <Box>
-      <Grid mt="15%" templateColumns="repeat(4, 250px)" gap="30px">
+      <Grid
+        mt="15%"
+        templateColumns={{ base: 'repeat(1, 250px)', md: 'repeat(4, 250px)' }}
+        gap="30px"
+      >
         {names.map((item, index) => (
           <GridItem>
             <NatureCard name={item} />
