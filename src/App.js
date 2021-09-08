3cacf0ae6b6b3760a3465e5a96ead2eccc5cb29d
Yara <yaraaseif@gmail.com>
2021-09-08 11:49:38 -0400

    added graphQL + authorization + database

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import { extendTheme } from '@chakra-ui/react';
 import { Logo } from './Logo';
 import NatureCardDisplay from './Components/NatureCardDisplay';
 import Header from './Components/Header';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { listTodos } from './graphql/queries';
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from './graphql/mutations';
+import { API, Storage } from 'aws-amplify';
 const theme2 = extendTheme({
   colors: {
     blue: {
@@ -31,11 +35,89 @@ const theme2 = extendTheme({
     },
   },
 });
+const initialFormState = { name: '', description: '', image: ''}
 
 function App() {
+
+  const [notes, setNotes] = React.useState([]);
+const [formData, setFormData] = React.useState(initialFormState);
+
+React.useEffect(() => {
+  fetchNotes();
+}, []);
+
+async function onChange(e) {
+  if (!e.target.files[0]) return
+  const file = e.target.files[0];
+  setFormData({ ...formData, image: file.name });
+  await Storage.put(file.name, file);
+  fetchNotes();
+}
+
+async function fetchNotes() {
+  const apiData = await API.graphql({ query: listTodos });
+  const notesFromAPI = apiData.data.listTodos.items;
+  await Promise.all(notesFromAPI.map(async note => {
+    if (note.image) {
+      const image = await Storage.get(note.image);
+      note.image = image;
+    }
+    return note;
+  }))
+  setNotes(apiData.data.listTodos.items);
+}
+
+async function createNote() {
+  if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
+  }
+  setNotes([ ...notes, formData ]);
+  setFormData(initialFormState);
+}
+
+async function deleteNote({ id }) {
+  const newNotesArray = notes.filter(note => note.id !== id);
+  setNotes(newNotesArray);
+  await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+}
   return (
     <ChakraProvider theme={theme2}>
       <Box textAlign="center" fontSize="xl" bg="blue.100">
+        <div>
+        <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <input
+  type="file"
+  onChange={onChange}
+/>
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+      <h2>{note.name}</h2>
+      <p>{note.description}</p>
+      <button onClick={() => deleteNote(note)}>Delete note</button>
+      {
+        note.image && <img src={note.image} style={{width: 400}} />
+      }
+    </div>
+          ))
+        }
+        </div>
+        </div>
         <Header />
         <Center>
           <NatureCardDisplay />
@@ -59,8 +141,9 @@ function App() {
           </VStack>
         </Grid> */}
       </Box>
+      <AmplifySignOut />
     </ChakraProvider>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
